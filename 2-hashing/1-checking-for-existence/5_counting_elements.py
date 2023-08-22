a4c2c0b4e7c07a64738b20559545b7de4c564f08
Matthew Benitez <matthewjbenitez95@gmail.com>
2023-08-22 14:19:12 -0700

    Add counting_elements.py

---
+++ 2-hashing/1-checking-for-existence/5_counting_elements.py
@@ -0,0 +1,34 @@
+# Given an integer array arr, count how many elements x there are
+# such that x + 1 is also in arr. If there are duplicates in arr, 
+# count them separately.
+
+
+TEST_CASES = [
+    [[0,1,2,3,4], 4],
+    [[0,1,2,3,4,3,2,1,0], 8],
+    [[0,1,2,3,4,5,4,3,2,1,0], 10],
+    [[0,2,4,2,0], 0],
+    [[-1,0,2,0], 1],
+    [[-1,0,-1,-1], 3],
+    [[1,1,3,3,5,5,7,7], 0]
+]
+
+def counting_elements(arr):
+    ans = 0
+    nums = {}
+    
+    for num in arr:
+        if num in nums:
+            nums[num] += 1
+        else:
+            nums[num] = 1
+
+    for num, count in nums.items():
+        if num + 1 in nums:
+            ans += count
+
+    return ans
+
+if __name__ == "__main__":
+    for case in TEST_CASES:
+        print(counting_elements(case[0]) == case[1])
