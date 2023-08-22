05d956af21d5ba06d6554ac5296294fbc849bbdb
Matthew Benitez <mbenitez@Matthews-MBP.lan>
2023-08-22 10:12:17 -0700

    Add sliding window approach

--- 1-arrays-and-strings/3-prefix-sum/5_k_radius_subarray_averages.py
+++ 1-arrays-and-strings/3-prefix-sum/5_k_radius_subarray_averages.py
@@ -38,8 +38,64 @@ def k_radius_subarray_averages(nums, k):
     
     return ans
 
-    # O(n) time and # O(n) space
+def k_radius_subarray_averages_optimized(nums, k):
+        # if radius is 0, average is just the element
+        if k == 0:
+            return nums
+
+        n = len(nums)
+        subarray_length = (2 * k) + 1
+        prefix_sum = [0] * (n + 1)
+        ans = [-1] * n
+
+        if subarray_length > n:
+            # no k-radius subarrays exist
+            return ans
+
+        # generate prefix_sums 
+        # 'prefix[i + 1]' will be sum of all elements of 'nums' from index '0' to 'i'.
+
+        for i in range(n):
+            prefix_sum[i + 1] = prefix_sum[i] + nums[i]
+
+        # iterate only on the k-radius subarrays
+        for i in range(k, n - k):
+            subarray_sum = prefix_sum[i + k + 1] - prefix_sum[i - k]
+            ans[i] = math.floor(subarray_sum / subarray_length)
+        
+        return ans
+
+def k_radius_subarray_sliding_window(nums, k):
+    n = len(nums)
+    ans = [-1] * n
+    window_size = (2 * k) + 1
+
+    # if looking at only 1 element, avg is just that element
+    if window_size == 1:
+        return nums
+
+    # if no k-radius subarrays exist, all values are -1
+    if window_size > n:
+        return ans
+
+    # build first window
+    cur_sum = sum(nums[:window_size])
+    ans[k] = cur_sum // window_size
+
+    # window will iterate at the right edge
+    # but answer will be updated at the middle
+    # start here --------v
+    # [7, 4, 3, 9, 1, 8, 5, 2, 6]
+    for i in range(window_size, n):
+        cur_sum = cur_sum - nums[i - window_size] + nums[i]
+        ans[i - k] = cur_sum // window_size
+
+    return ans
 
 if __name__ == "__main__":
     for case in TEST_CASES:
-        print(k_radius_subarray_averages(case[0], case[1]) == case[2])
+        print(
+            k_radius_subarray_averages(case[0], case[1]) == case[2], 
+            k_radius_subarray_averages_optimized(case[0], case[1]) == case[2],
+            k_radius_subarray_sliding_window(case[0], case[1]) == case[2]
+        )
