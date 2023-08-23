1bb98d571957627e0f14083851e24a2923769b7d
Matthew Benitez <matthewjbenitez95@gmail.com>
2023-08-23 16:00:30 -0700

    Add ransom_note.py

---
+++ 2_hashing/3_more_hashing_examples/4_ransom_note.py
@@ -0,0 +1,47 @@
+# Given two strings ransomNote and magazine, return true if 
+# ransomNote can be constructed by using the letters from 
+# magazine and false otherwise.
+
+# Each letter in magazine can only be used once in ransomNote.
+
+TEST_CASES = [
+    ['a', 'b', False],
+    ['aa', 'ab', False],
+    ['aa', 'aab', True],
+    [
+        'franklymydearidontgiveadamn', 
+        'thereareagreatmanythingswhichidonotknoworconsidermyselftobeverymuchanexpertatbutchiefamongthemisdeath', 
+        True
+    ],
+    [
+        'iamlordvoldemort', 
+        'tommarvoloriddle', 
+        True
+    ],
+    [
+        'theboywholived', 
+        'tommarvoloriddle', 
+        False
+    ],
+]
+
+def ransom_note(note, magazine):
+    note_letters = {}
+    for letter in note:
+        if letter not in note_letters:
+            note_letters[letter] = 0
+        
+        note_letters[letter] += 1
+
+    for letter in magazine:
+        if letter in note_letters:
+            note_letters[letter] -= 1
+            if note_letters[letter] == 0:
+                note_letters.pop(letter)
+
+    return len(note_letters) == 0
+
+
+if __name__=="__main__":
+    for case in TEST_CASES:
+        print(ransom_note(case[0], case[1]) == case[2])
